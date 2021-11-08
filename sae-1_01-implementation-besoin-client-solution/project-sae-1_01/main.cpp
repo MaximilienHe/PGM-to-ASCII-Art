401bb10ed51b565c4e9338819ccf80440b81a578
Florian Mure <mure.florian2@gmail.com>
2021-11-08 22:15:16 +0100

    Initialisation du dépôt github pour la SAE 1.01 Implémentation d'un besoin client

---
+++ sae-1_01-implementation-besoin-client-solution/project-sae-1_01/main.cpp
@@ -0,0 +1,16 @@
+#ifdef _WIN32
+#include <windows.h>
+#endif // Win 32
+
+#include <iostream>
+
+int main()
+{
+#ifdef _WIN32
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+#endif // WIN 32
+
+
+    return 0;
+}
