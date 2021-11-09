f944a4720ae51ebd94092355f497c37d93c6f248
Florian Mure <71829477+Florian-Mure@users.noreply.github.com>
2021-11-09 19:01:57 +0100

    Modification commentaire

--- sae-1_01-implementation-besoin-client-solution/project-sae-1_01/main.cpp
+++ sae-1_01-implementation-besoin-client-solution/project-sae-1_01/main.cpp
@@ -16,23 +16,23 @@ int main()
     //Ouverture du fichier "image2.pgm" issu du dossier "ImagePGM" en mode binaire :
     std::ifstream fichier("../ImagePGM/image2.pgm", std::ios_base::binary);
 
-    // CrÈation du fichier image2.txt pour stocker l'image finale
+    // Cr√©ation du fichier image2.txt pour stocker l'image finale
     std::ofstream exportFichier("image-exporter.txt");
 
     // Decodage de l'entete dans un tableau taille
     std::vector<double> taille = decodageEntete(fichier);
 
-    // Recuperation des donnÈes de l'entete et stockage dans des variables
+    // Recuperation des donn√©es de l'entete et stockage dans des variables
     std::vector<double> metaDonnes = recuperationMetadonnes(taille);
     double tailleImage = metaDonnes[0], largeur = metaDonnes[1], hauteur = metaDonnes[2];
 
-    // Recuperation des donnÈes du fichier
+    // Recuperation des donn√©es du fichier
     std::vector<std::vector<int>> donnees = recuperationDonneesDuFichier(fichier, tailleImage, hauteur, largeur);
 
-    // On parcourt les valeurs du vecteur donnees, et on les compare pour savoir si elles correspondent ‡ W, w, l ...
+    // On parcourt les valeurs du vecteur donnees, et on les compare pour savoir si elles correspondent √† W, w, l ...
     std::vector<std::vector<std::string>> donneesEnAscii = convertirAvecPalette(donnees, tailleImage, hauteur, largeur);
 
-    // On parcourt le tableau donneesEnAscii pour afficher les valeurs, et afficher un \n si on atteint la taille de la largeur
+    // On parcourt le tableau donneesEnAscii pour cr√©er un fichier texte qui contient l'image
     exportImageAsciiEnTxt(donneesEnAscii, exportFichier);
 
     return 0;
