ec08bb928fdd12a5d5c00f13c7cc17d27ab3539d
Maximilien Herr <Maximilien.HERR@etu.uca.fr>
2021-11-09 16:58:25 +0100

    Version 2 : suppresion de l'affichage console && export de l'affichage dans un fichier text renommable en début

--- sae-1_01-implementation-besoin-client-solution/Tests/tests.cpp
+++ sae-1_01-implementation-besoin-client-solution/Tests/tests.cpp
@@ -15,8 +15,11 @@ int main()
     SetConsoleOutputCP(CP_UTF8);
 #endif // WIN 32
 
-    //Ouverture du fichier "image1.pgm" issu du dossier "ImagePGM" en mode binaire :
-    std::ifstream fichier("../ImagePGM/image1.pgm", std::ios_base::binary);
+    //Ouverture du fichier "imagetest.pgm" issu du dossier "ImagePGM" en mode binaire :
+    std::ifstream fichier("../ImagePGM/imagetest.pgm", std::ios_base::binary);
+
+    // Création du fichier image2.txt pour stocker l'image finale
+    std::ofstream exportFichier("imagetest.txt");
 
     //Décodage entête du fichier (4 lignes) dans un string "ligne"
     std::string ligne;
@@ -26,7 +29,6 @@ int main()
     // On parcourt les 4 premières lignes, et lorsqu'on atteint la troisième (qui contient les tailles de pixels) on la stocke dans un string spécifique
     for (size_t i = 0; i < 4; i++) {
         std::getline(fichier, ligne);
-        std::cout << ligne << "\n";
         // 3e ligne atteinte, on récupère les 2 valeurs de taille (largeur / hauteur) dans un vector
         if (i == 2) {
             std::stringstream ligneTailleInter(ligne);
@@ -38,7 +40,7 @@ int main()
     }
   
     // A partir du vecteur, on calcule la largeur * hauteur dans la variable tailleImage, et on stocke la largeur et la hauteur
-    double tailleImage = 1, largeur, hauteur;
+    double tailleImage = 1, largeur = 0, hauteur = 0;
     size_t cptTaille = 0;
     for (auto valeurs : taille) {
         tailleImage *= valeurs;
@@ -50,10 +52,6 @@ int main()
         }
         cptTaille += 1;
     }
-    std::cout << tailleImage;
-  
-    //Ouverture du fichier "ImageTest1.pgm" en mode binaire :
-    std::ifstream fichier("../ImagePGM/image1.pgm", std::ios_base::binary);
 
     //Création d'une mémoire de tailleImage octets :
     std::vector<char> donneesChar(tailleImage);
@@ -108,10 +106,10 @@ int main()
     // On parcourt le tableau donneesEnAscii pour afficher les valeurs, et afficher un \n si on atteint la taille de la largeur
     size_t cptAffichage = 0;
     for (auto affichagefinal : donneesEnAscii) {
-        std::cout << affichagefinal;
+        exportFichier << affichagefinal;
         cptAffichage += 1;
         if (cptAffichage == largeur) {
-            std::cout << "\n";
+            exportFichier << "\n";
             cptAffichage = 0;
         }
     }
