e47590f9c1650f6daedd400d6b5f98a8c87ce297
Maximilien Herr <Maximilien.HERR@etu.uca.fr>
2021-11-09 16:21:35 +0100

    Récupération valeurs de taille (largeur, hauteur et multiplication des deux) à partir de l'entête && Parcours des valeurs reçues en binaire (0 à 255) et comparaison pour push_back la palette de couleur dans le tableau des valeurs Ascii final, en fonction de la valeur des caractères && affichage du tableau final dans la console

--- sae-1_01-implementation-besoin-client-solution/Tests/tests.cpp
+++ sae-1_01-implementation-besoin-client-solution/Tests/tests.cpp
@@ -2,7 +2,11 @@
 #include <windows.h>
 #endif // Win 32
 
+#include <fstream>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
 
 int main()
 {
@@ -11,6 +15,80 @@ int main()
     SetConsoleOutputCP(CP_UTF8);
 #endif // WIN 32
 
+    //Ouverture du fichier "image1.pgm" issu du dossier "ImagePGM" en mode binaire :
+    std::ifstream fichier("../ImagePGM/image1.pgm", std::ios_base::binary);
 
-    return 0;
+    //Décodage entête du fichier (4 lignes) dans un string "ligne"
+    std::string ligne;
+    std::string ligneTaille;
+    std::vector <double> taille;
+    taille.reserve(2);
+    // On parcourt les 4 premières lignes, et lorsqu'on atteint la troisième (qui contient les tailles de pixels) on la stocke dans un string spécifique
+    for (size_t i = 0; i < 4; i++) {
+        std::getline(fichier, ligne);
+        std::cout << ligne << "\n";
+        // 3e ligne atteinte, on récupère les 2 valeurs de taille (largeur / hauteur) dans un vector
+        if (i == 2) {
+            std::stringstream ligneTailleInter(ligne);
+            while (!ligneTailleInter.eof()) {
+                std::getline(ligneTailleInter, ligneTaille, ' ');
+                taille.push_back(std::stod(ligneTaille));
+            }
+        }
+    }
+    // A partir du vecteur, on calcule la largeur * hauteur dans la variable tailleImage, et on stocke la largeur et la hauteur
+    double tailleImage = 1, largeur, hauteur;
+    size_t cptTaille = 0;
+    for (auto valeurs : taille) {
+        tailleImage *= valeurs;
+        if (cptTaille == 0) {
+            largeur = valeurs;
+        }
+        else {
+            hauteur = valeurs;
+        }
+        cptTaille += 1;
+    }
+    std::cout << tailleImage;
+
+    // On parcourt les valeurs du vecteur donnees, et on les compare pour savoir si elles correspondent à W, w, l ...
+    std::vector<std::string> donneesEnAscii;
+    donneesEnAscii.reserve(tailleImage);
+    for (auto parcourir : donnees) {
+        if (parcourir <= 31) {
+            donneesEnAscii.push_back("W");
+        }
+        else if (parcourir <= 63) {
+            donneesEnAscii.push_back("w");
+        }
+        else if (parcourir <= 95) {
+            donneesEnAscii.push_back("l");
+        }
+        else if (parcourir <= 127) {
+            donneesEnAscii.push_back("i");
+        }
+        else if (parcourir <= 159) {
+            donneesEnAscii.push_back(":");
+        }
+        else if (parcourir <= 191) {
+            donneesEnAscii.push_back(",");
+        }
+        else if (parcourir <= 223) {
+            donneesEnAscii.push_back(".");
+        }
+        else if (parcourir <= 255) {
+            donneesEnAscii.push_back(" ");
+        }
+    }
+
+    // On parcourt le tableau donneesEnAscii pour afficher les valeurs, et afficher un \n si on atteint la taille de la largeur
+    size_t cptAffichage = 0;
+    for (auto affichagefinal : donneesEnAscii) {
+        std::cout << affichagefinal;
+        cptAffichage += 1;
+        if (cptAffichage == largeur) {
+            std::cout << "\n";
+            cptAffichage = 0;
+        }
+    }
 }
--- sae-1_01-implementation-besoin-client-solution/project-sae-1_01/main.cpp
+++ sae-1_01-implementation-besoin-client-solution/project-sae-1_01/main.cpp
@@ -11,15 +11,5 @@ int main()
     SetConsoleOutputCP(CP_UTF8);
 #endif // WIN 32
 
-    //Ouverture du fichier "image1.pgm" en mode binaire :
-    std::ifstream fichier("image1.pgm", std::ios_base::binary);
-    //Ici vous devez décoder l'entête du fichier
-    //...
-    //Création d'une mémoire de 10 octets :
-    std::vector<char> donnees(10);
-    //Lecture de 10 octets depuis le fichier et stockage dans le tableau donnees :
-    fichier.read(donnees.data(), 10);
-
-
     return 0;
 }
